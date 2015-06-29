873b5eef995e0049a6a2a0a8ff433ae0d721778d
Torrance Davenport <torrance@fragmentlabs.com>
2015-06-28 22:06:05 -0400

    removing quote

--- Src/Vince.php
+++ Src/Vince.php
@@ -11,7 +11,7 @@ class Vince
             'You\'re going to be in a great mood all day, because you\'re going be slapping your troubles away with the SlapChop',
             'You’re going to love my nuts',
             'It\'s easy, you just got one hand, and chop.',
-            'Look at this you’re going to have an exciting life now.',
+            'Look at this you\'re going to have an exciting life now.',
             'So easy, one finger, if I can do it with one finger you guys can do it with your whole hand.',
             'Alright life\'s hard enough as it is. You don\'t wanna cry anymore.'
         ];
